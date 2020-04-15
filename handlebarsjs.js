015f1a4389bef647593205c6c2dd577c926b8ba0
SAVAN VEKARIYA <savanvekariya.sv@gmail.com>
2020-04-15 19:23:49 +0530

    first commit 15-4-2020

---
+++ handlebarsjs.js
@@ -0,0 +1,14 @@
+var client = contentful.createClient({
+   space: "aitbdbca11xl",
+   accessToken: "pZW1dRIvza3VmJ6hxXBCoohUUjomMDqudntJFbv8kbY",
+});
+
+var renderTemplates = (templateId, target) => {
+   client.getEntries().then((entries) => {
+      var templateTitles = $(templateId).html();
+      var payload = Handlebars.compile(templateTitles)(entries);
+      //console.log(entries);
+      $(target).append(payload);
+   });
+};
+renderTemplates("#templateTitles", "#author-list");
